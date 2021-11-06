0d710e93e3691bb8a6ea81c3324715188a17fb58
Shen Sat <shensat@shens-mbp.home>
2021-11-06 13:21:08 +0000

    Refactor query functionality

--- pieces.lua
+++ pieces.lua
@@ -20,18 +20,12 @@ bin.activate = function(self,pointer,built_pieces)
     if do_pieces_match(bp, local_bin, false) then del(built_pieces,bp) end
   end  
 end
-
-query = {
-  sprite = 4,
-  width = 8,
-  x,
-  y,
-  activate = function(self,pointer,built_pieces)
-    local local_query = copy_piece(self,pointer)
-    for bp in all(built_pieces) do
-      if do_pieces_match(bp, local_query, false) then top_text:assign(bp.text) end
-    end
+query = create_piece(4,8)
+query.activate = function(self,pointer,built_pieces)
+  local local_query = copy_piece(self,pointer)
+  for bp in all(built_pieces) do
+    if do_pieces_match(bp, local_query, false) then top_text:assign(bp.text) end
   end
-}
+end
 
 pieces = { road, hut, bin, query }
